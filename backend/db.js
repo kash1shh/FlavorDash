99ab28ee495d1330a8a4cffde13bd76b78593ceb
Kashish <kashishparmar2205@gmail.com>
2024-06-28 22:30:31 +0530

    another commit

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
-const mongoURI ="mongodb+srv://Flavordash:Twenty204.@cluster0.b2pzsva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongoURI ="mongodb+srv://Flavordash:Twenty204.@cluster0.b2pzsva.mongodb.net/flavordash_mern?retryWrites=true&w=majority&appName=Cluster0"
 
 const mongoDB = async ()=>{
     await mongoose.connect(mongoURI, {useNewUrlParser: true }, async(err, result)=>{
         if(err) console.log("error---", err)
             else{
                 console.log("connected successfully");
+                const fetched_data = await mongoose.connection.db.collection("flavordash_data");
+                fetched_data.find({}).toArray(function(err, data){
+                    if(err) console.log(err);
+                    else console.log(data);
+                })
         }
     });    
 }
--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 const port = 5000
 const mongoDB = require("./db");
-mongoDB();
 
 app.get('/', (req, res) => {
     res.send("Hello world");
 })
+app.use(express.json());
 
+app.use('/api',require("./routes/createUser "));
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
